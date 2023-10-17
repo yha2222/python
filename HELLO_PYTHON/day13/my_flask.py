dde5462f34fb6e5487afe0010ec523036a4d3a3b
yha2222 <yhaha5751@gmail.com>
2023-10-17 14:00:52 +0900

    1017 CRUD

--- HELLO_PYTHON/day13/my_flask.py
+++ HELLO_PYTHON/day13/my_flask.py
@@ -19,9 +19,41 @@ def mem_selects():
 def mem_select():
     params = request.get_json()
     m_id = params['m_id']
-    de = DaoMember()
-    vo = de.selectOne(m_id)
+    dm = DaoMember()
+    vo = dm.selectOne(m_id)
     return jsonify({'vo': vo})
+
+@app.route('/mem_add', methods=['POST'])
+def mem_add():
+    params = request.get_json()
+    m_name = params['m_name']
+    tel = params['tel']
+    email = params['email']
+    
+    dm = DaoMember()
+    cnt = dm.insert(m_name, tel, email)
+    return jsonify({'cnt': cnt})
+
+@app.route('/mem_mod', methods=['POST'])
+def mem_mod():
+    params = request.get_json()
+    m_id = params['m_id']
+    m_name = params['m_name']
+    tel = params['tel']
+    email = params['email']
+    
+    dm = DaoMember()
+    cnt = dm.update(m_id, m_name, tel, email)
+    return jsonify({'cnt': cnt})
+
+@app.route('/mem_del', methods=['POST'])
+def mem_del():
+    params = request.get_json()
+    m_id = params['m_id']
+    
+    dm = DaoMember()
+    cnt = dm.delete(m_id)
+    return jsonify({'cnt': cnt})
 
 if __name__ == '__main__':
     app.run(debug=True)
